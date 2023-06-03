0ec3c9001b20594d936e18053d9f158cf49aa687
Rhauanne Castro <100003325+rhauannecastro@users.noreply.github.com>
2023-06-03 20:37:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sim(){
+    alert("Deposite agora, estou na sua porta esperando!!")
+}
+    function desvia(){
+    var btn = document.getElementById("btn");
+    console.log(btn);
+    console.log("OPS!");
+}
